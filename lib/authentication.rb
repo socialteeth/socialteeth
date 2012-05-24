7267a2f5c9041a30786580ac3f90b5c01a5fb7bd
Daniel MacDougall <dmacdougall@gmail.com>
2012-05-24 13:33:32 -0700

    Move authentication routes into library

---
+++ lib/authentication.rb
@@ -0,0 +1,62 @@
+class SocialTeeth < Sinatra::Base
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
+    errors = []
+
+    errors = enforce_required_params(:name, :email, :password, :confirm_password)
+
+    if params[:email]
+      existing_user = User.find(:email => params[:email])
+      errors << "Another user exists with that email." if existing_user
+    end
+
+    errors << "Passwords must match." unless params[:password] == params[:confirm_password]
+
+    if errors.empty?
+      self.current_user =
+          User.create(:name => params[:name], :email => params[:email], :password => params[:password])
+      redirect "/"
+    else
+      flash[:errors] = errors
+      redirect "/signup"
+    end
+    redirect "/signup"
+  end
+
+  get "/signin" do
+    erb :signin
+  end
+
+  post "/signin" do
+    errors = enforce_required_params(:email, :password)
+
+    requested_user = User.find(:email => params[:email])
+    if requested_user && requested_user.password == params[:password]
+      self.current_user = requested_user
+      redirect "/"
+    else
+      errors << "Invalid login." if errors.empty?
+      flash[:errors] = errors
+      redirect "/signin"
+    end
+  end
+
+  get "/signout" do
+    session.clear
+    redirect "/"
+  end
+
+  def current_user()
+    @current_user ||= User.find(:email => session[:email])
+  end
+
+  def current_user=(user)
+    return if session[:email] == user.email
+    @current_user = nil
+    session.clear
+    session[:email] = user.email if user
+  end
+end
