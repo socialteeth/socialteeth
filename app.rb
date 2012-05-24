9b938a7493b72e261949249167d75d216566f798
Daniel MacDougall <dmacdougall@gmail.com>
2012-05-24 02:47:28 -0700

    Create new user or expose errors when posting signup form

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require "bundler/setup"
 require "pathological"
 require "sinatra"
 require "coffee-script"
+require "rack-flash"
+require "lib/models"
 
 class SocialTeeth < Sinatra::Base
+  enable :sessions
+  use Rack::Flash, :sweep => true
+
   def initialize(pinion)
     @pinion = pinion
     super
@@ -18,7 +23,25 @@ class SocialTeeth < Sinatra::Base
   end
 
   post "/signup" do
-    # Create user
-    redirect "/"
+    errors = []
+
+    empty_fields = [:name, :email, :password, :confirm_password].select { |field| params[field].empty? }
+    errors << "All fields are required." unless empty_fields.empty?
+
+    if params[:email]
+      existing_user = User.find(:email => params[:email])
+      errors << "Another user exists with that email." if existing_user
+    end
+
+    errors << "Passwords must match." unless params[:password] == params[:confirm_password]
+
+    if errors.empty?
+      User.create(:name => params[:name], :email => params[:email], :password => params[:password])
+      redirect "/"
+    else
+      flash[:errors] = errors
+      redirect "/signup"
+    end
+    redirect "/signup"
   end
 end
