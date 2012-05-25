48507b7fae08a3637fe8c8016ca21d46f5ebdef7
Daniel MacDougall <dmacdougall@gmail.com>
2012-05-24 18:31:06 -0700

    Add creation of an ad to seed_db_content

--- script/seed_db_content.rb
+++ script/seed_db_content.rb
@@ -8,3 +8,15 @@ demo_user = User.find(:email => "demo@socialteeth.org")
 if demo_user.nil?
   demo_user = User.create(:name => "Demo User", :email => "demo@socialteeth.org", :password => "demo")
 end
+
+if demo_user.ads.empty?
+  description = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin congue pharetra orci in " +
+    "egestas. Vestibulum sed orci est, at sollicitudin nunc. Phasellus vitae euismod lorem. Nulla " +
+    "venenatis dignissim felis, vitae tincidunt erat egestas sit amet. In hac habitasse platea dictumst. " +
+    "Quisque turpis purus, faucibus quis cursus et, gravida nec felis. Quisque lacinia malesuada risus a " +
+    "dapibus."
+
+  Ad.create(:title => "Demo User Ad", :description => description, :user_id => demo_user.id, :goal => 1000,
+      :ad_type => "video", :url => "http://example.com", :thumbnail_url => "http://example.com/thumbnail.png",
+      :deadline => Time.now + 60 * 60 * 24 * 30)
+end
