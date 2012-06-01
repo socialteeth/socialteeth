dde3426521422dc4daca485fef3459f22eca7507
Daniel MacDougall <dmacdougall@gmail.com>
2012-06-01 10:20:45 -0700

    Generate resized and cropped thumbnails

--- lib/uploader.rb
+++ lib/uploader.rb
@@ -1,4 +1,5 @@
 require "fileutils"
+require "image_science"
 
 class Uploader
   UPLOAD_URL_BASE = File.join(File.dirname(File.dirname(__FILE__)), "public", "uploads")
@@ -6,13 +7,21 @@ class Uploader
   # Uploads a file to socialteeth/public/uploads.
   # TODO: This should upload to S3 in production and the filesystem in development.
   def upload_ad_thumbnail(ad, thumbnail_file)
-    ad_directory = FileUtils.mkdir_p(File.join(UPLOAD_URL_BASE, "ads", "#{ad.id}")).first
-    thumbnail_destination = File.join(ad_directory, "thumbnail")
+    ad_directory = FileUtils.mkdir_p(File.join(UPLOAD_URL_BASE, "ad", "#{ad.id}")).first
+    thumbnail_destination = File.join(ad_directory, "thumbnail_original.png")
     File.open(thumbnail_destination, "w") do |destination_file|
       while block = thumbnail_file.read(65536)
         destination_file.print block
       end
     end
+
+    thumbnail_200_destination = File.join(ad_directory, "thumbnail_200.png")
+    thumbnail_200_cropped_destination = File.join(ad_directory, "thumbnail_200_cropped.png")
+    ImageScience.with_image(thumbnail_destination) do |image|
+      image.thumbnail(200) { |thumb| thumb.save(thumbnail_200_destination) }
+      image.cropped_thumbnail(200) { |thumb| thumb.save(thumbnail_200_cropped_destination) }
+    end
+
     "/#{thumbnail_destination.split("/").drop_while { |directory| directory != "uploads"}.join("/")}"
   end
 end
