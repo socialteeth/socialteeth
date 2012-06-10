ed1303b9a4112ca3fa06237761f1ed8b29d3cf27
Daniel MacDougall <dmacdougall@gmail.com>
2012-06-10 03:53:46 -0700

    Include DB_USER and DB_PASS when running migrations

--- script/run_migrations.rb
+++ script/run_migrations.rb
@@ -5,5 +5,5 @@ require "pathological"
 require "environment"
 
 command = "bundle exec sequel -m migrations/"
-db_url = "postgres://#{DB_HOST}/#{DB_NAME}"
+db_url = "postgres://#{DB_USER}:#{DB_PASS}@#{DB_HOST}/#{DB_NAME}"
 puts `#{command} #{db_url}`
