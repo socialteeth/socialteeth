87a220c15d8fdff44f39e6f14db6b932e187e112
Daniel MacDougall <dmacdougall@gmail.com>
2012-06-16 22:41:17 -0700

    Enable support for YouTube or Vimeo URLs

--- models/ad.rb
+++ models/ad.rb
@@ -1,3 +1,5 @@
+require "opengraph"
+
 class Ad < Sequel::Model
   many_to_one :user
 
@@ -9,5 +11,20 @@ class Ad < Sequel::Model
       :cropped => false
     }.merge(options)
     File.join(self.thumbnail_url_base, "thumbnail_#{options[:size]}#{options[:cropped] ? "_cropped" : ""}.png")
+  end
+
+  def video_embed_url
+    movie = OpenGraph.fetch(self.url)
+    return "" unless movie
+
+    url = URI.parse(movie.url)
+    if url.host.include?("youtube")
+      # http://www.youtube.com/watch?v=a1b2c3
+      "http://www.youtube.com/embed/#{url.query.split("=")[1]}"
+    elsif url.host.include?("vimeo")
+      # http://vimeo.com/123456
+      "http://player.vimeo.com/video/#{url.path.gsub("/", "")}"
+    else ""
+    end
   end
 end
