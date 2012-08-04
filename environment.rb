b54ed179531ca78a34f3a2f07e8f2b171b894d8c
Daniel MacDougall <dmacdougall@gmail.com>
2012-08-04 15:57:28 -0700

    Working end-to-end test payment

--- environment.rb
+++ environment.rb
@@ -3,3 +3,5 @@ DB_HOST = "localhost"
 DB_NAME = "socialteeth"
 DB_USER = "postgres"
 DB_PASS = ""
+
+STRIPE_TEST_SECRET_KEY = "sk_07TbSqkx2eLvcLM44lka19BD8ldc1"
--- lib/contribute.rb
+++ lib/contribute.rb
@@ -5,13 +5,44 @@ class SocialTeeth < Sinatra::Base
     erb :contribute, :locals => { :ad => ad }
   end
 
+  get "/ads/:id/contribute_success" do
+    ensure_signed_in
+    halt 404 unless ad = Ad.find(:public_id => params[:id])
+    erb :contribute_success, :locals => { :ad => ad }
+  end
+
   post "/ads/:id/contribute" do
     ensure_signed_in
     halt 404 unless ad = Ad.find(:public_id => params[:id])
-    errors = enforce_required_params([:contribution_amount, :stripe_token])
-    errors << "Invalid dollar amount" unless params[:contribution_amount].is_currency?
+    errors = enforce_required_params([:dollar_amount, :cent_amount, :stripe_token])
+    begin
+      dollars = Integer(params[:dollar_amount])
+      cents = Integer(params[:cent_amount])
+      if params[:cent_amount] && params[:cent_amount].size != 2
+        errors << "Invalid contribution amount: $#{dollars}.#{cents}"
+      end
+    rescue ArgumentError => error
+      errors << "Invalid contribution amount"
+    end
 
-    flash[:errors] = errors
-    redirect "/ads/#{ad.public_id}/contribute"
+    if errors.empty?
+      Stripe.api_key = STRIPE_TEST_SECRET_KEY
+
+      token = params[:stripe_token]
+      amount_in_cents = dollars * 100 + cents
+
+      # Create the charge on Stripe's servers - this will charge the user's card
+      charge = Stripe::Charge.create(
+        :amount => amount_in_cents,
+        :currency => "usd",
+        :card => token,
+        :description => "#{current_user.email} -- #{ad.title} -- $#{dollars}.#{cents}"
+      )
+
+      redirect "/ads/#{ad.public_id}/contribute_success"
+    else
+      flash[:errors] = errors
+      redirect "/ads/#{ad.public_id}/contribute"
+    end
   end
 end
