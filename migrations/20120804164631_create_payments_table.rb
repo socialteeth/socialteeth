667a278c899e3965d0374f69deb215afa743eed7
Chris Fedor <raulfoo@Chris-Fedors-MacBook-Pro.local>
2012-09-27 16:29:11 -0700

    remove signin requirement

--- migrations/20120804164631_create_payments_table.rb
+++ migrations/20120804164631_create_payments_table.rb
@@ -2,14 +2,14 @@ Sequel.migration do
   change do
     create_table :payments do
       primary_key :id
-      foreign_key :user_id, :users, :null => false
+      String :email_id, :users, :null => false
       foreign_key :ad_id, :ads, :null => false
       String :public_id, :size => 64, :null => false
       Integer :amount, :null => false
       Boolean :refunded, :null => false, :default => false
       DateTime :created_at
       DateTime :refunded_at
-      index :user_id
+      index :email_id
       index :ad_id
       index :created_at
     end
