737cdb4fd8a8ebee09b5ffb12e2b19e0df340875
Daniel MacDougall <dmacdougall@gmail.com>
2012-10-01 14:26:17 -0700

    Ability to pass a migration id to run_migrations

--- script/run_migrations.rb
+++ script/run_migrations.rb
@@ -4,6 +4,6 @@ require "bundler/setup"
 require "pathological"
 require "environment"
 
-command = "bundle exec sequel -m migrations/"
+command = "bundle exec sequel -m migrations/ #{"-M #{ARGV[0]}" if ARGV[0]}"
 db_url = "postgres://#{DB_USER}:#{DB_PASS}@#{DB_HOST}/#{DB_NAME}"
 puts `#{command} #{db_url}`
